727c4e9bf76a5f4c687344d1c9ce2ec20c70f2a2
Manuel Streuhofer <manuel@streuhofer.net>
2016-04-23 20:21:57 +0200

    Initial upload

--- README.md
+++ README.md
@@ -1,1 +1,129 @@
 # wznat
+
+`wznat`, pronounced *whiz-nat*. NAT auto-configuration. Local DNS and DHCP
+service provided by `dnsmasq`. Works on Linux and OS X.
+
+Configure all locally running LXC container, VirtualBox machines and possibly
+LXC container within VirtualBox machines with ease by using DHCP, yet have all
+these machines refer to each other by name and have access to the outside world
+thru the physical host machine's external network interface. No more thinking
+about which IP address to give to which machine, editing any `/etc/hosts` file
+or other shenanigans.
+
+`dnsmasq` will provide DNS for the domain `.wznat` on `IF_LOCAL` and all
+`IF_INTERNAL` interfaces as well as DHCP on all `IF_INTERNAL` interfaces. NAT
+between `IF_INTERNAL` and `IF_EXTERNAL` will be handled by `iptables` on Linux
+and `pf` on OS X.
+
+The first half of the /16 subnet (`x.x.0.1 - x.x.127.255`) as configured on
+`IF_INTERNAL` ist setup as static DHCP range while the second half
+(`x.x.128.1 - x.x.255.254`) is setup as dynamic DHCP range.
+
+Hardcoded defaults:
+
+|              | Linux | OS X    |
+| :---         | :---  | :---    |
+| IF\_LOCAL    | lo    | lo0     |
+| IF\_EXTERNAL | eth0  | en0     |
+| IF\_INTERNAL | wznat | vboxnet |
+| VBOX\_USER   | -     | -       |
+
+Local defaults can be specified in `/etc/wznat/setup.conf`.
+
+## OS X
+
+Install necessary software (you will need [Homebrew] (http://brew.sh) for
+this): `brew install dnsmasq`.
+
+Symlink like you never symlinked before:
+
+```
+/etc/wznat -> $repo-checkout
+/etc/resolver/wznat -> /etc/wznat/resolver.conf
+/Library/LaunchDaemons/wznat.plist -> /etc/wznat/setup.plist
+```
+
+All `*.plist` files within the directory `/etc/wznat` have to be owned by
+`root`. Make sure to set the proper ownership.
+
+There is no such thing as LXC on OS X. It is all about VirtualBox. The script
+`setup.sh` will use `VBoxManage` to find out about your VirtualBox
+configuration. It will create a host-only network if there is none. It will
+deactivate the associated VirtualBox DHCP server. Finally it will activate the
+necessary network interfaces.
+
+Setup is done on boot as system user `root`. It uses `sudo` to configure
+VirtualBox for your user. This is the reason why you have to **specify your
+username** in the variable `VBOX_USER` within `/etc/wznat/setup.conf`.
+
+Configure your VirtualBox machine to have it's Network Adapter 1 attached to
+Host-only Adapter `vboxnet0`. Set Promiscuous Mode under Advanced to *Allow
+All*.
+
+## Linux
+
+Install necessary software `iptables` and `dnsmasq`.
+
+Symlink like you never symlinked before:
+
+```
+/etc/wznat -> $repo-checkout
+/etc/network/if-up.d/wznat -> /etc/wznat/setup.sh
+/etc/network/if-down.d/wznat -> /etc/wznat/setup.sh
+/etc/dnsmasq.d/wznat -> /etc/wznat/dnsmasq.conf
+```
+
+Configure your VirtualBox machine to have it's Network Adapter 1 attached to
+Bridged Adapter `wznat0`. Set Promiscuous Mode under Advanced to *Allow All*.
+
+### /etc/network/interfaces
+
+```
+auto wznat0
+iface wznat0 inet static
+  bridge_ports none
+  bridge_fd 0
+  bridge_maxwait 0
+  bridge_stp off
+  address 172.16.0.1/16
+
+auto wznat1
+iface wznat0 inet static
+  bridge_ports none
+  bridge_fd 0
+  bridge_maxwait 0
+  bridge_stp off
+  address 172.17.0.1/16
+```
+
+### /etc/lxc/default.conf
+
+```
+lxc.network.type = veth
+lxc.network.link = wznat1
+lxc.network.flags = up
+```
+
+### /etc/dhcp/dhclient.conf
+
+```
+prepend domain-name-servers 127.0.0.1;
+prepend domain-search "wznat";
+```
+
+# LICENSE
+
+Copyright (c) 2016, Manuel Streuhofer <manuel@streuhofer.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ setup.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2016, Manuel Streuhofer <manuel@streuhofer.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+IF_LOCAL="lo"
+IF_EXTERNAL="eth0"
+IF_INTERNAL="wznat"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	PATH="$PATH:/usr/local/bin"
+
+	[ -z "$(command -v VBoxManage)" ] && exit 0
+
+	IF_LOCAL="lo0"
+	IF_EXTERNAL="en0"
+	IF_INTERNAL="vboxnet"
+	VBOX_USER=""
+
+	MODE="start"
+	IFACE="vboxnet0"
+	IF_ADDRESS="172.16.0.1"
+	IF_NETMASK="255.255.0.0"
+fi
+
+LOCAL_CONFIG="/etc/wznat/setup.conf"
+[ -f "$LOCAL_CONFIG" ] && source "$LOCAL_CONFIG"
+
+CONFIGD="/etc/wznat/config.d"
+[ ! -d "$CONFIGD" ] && exit 1
+
+function setup_configd {
+	CONFIGD_HOSTS="$CONFIGD/dnsmasq.hosts"
+	CONFIGD_IFACE_LOCAL="$CONFIGD/dnsmasq.$IF_LOCAL.iface"
+	CONFIGD_IFACE="$CONFIGD/dnsmasq.$IFACE.iface"
+	CONFIGD_PF="$CONFIGD/pf.rules"
+}
+
+function cleanup_configd {
+	setup_configd
+
+	touch "$CONFIGD_HOSTS"
+
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		sed -i '' "/$IFACE$/d" "$CONFIGD_HOSTS"
+		sed -i '' "/^$IF_ADDRESS/d" "$CONFIGD_HOSTS"
+	else
+		sed -i "/$IFACE$/d" "$CONFIGD_HOSTS"
+		sed -i "/^$IF_ADDRESS/d" "$CONFIGD_HOSTS"
+	fi
+
+	[ -f "$CONFIGD_IFACE_LOCAL" ] && rm "$CONFIGD_IFACE_LOCAL"
+	[ -f "$CONFIGD_IFACE" ] && rm "$CONFIGD_IFACE"
+	[ -f "$CONFIGD_PF" ] && rm "$CONFIGD_PF"
+
+	for CLEANUP_IFACE_FILE in $(find "$CONFIGD" -type f -name 'dnsmasq.*.iface'); do
+		CLEANUP_IFACE_NAME="$(basename "$CLEANUP_IFACE_FILE" | cut -d'.' -f2)"
+		ifconfig "$CLEANUP_IFACE_NAME" >/dev/null 2>&1 && continue
+		rm "$CLEANUP_IFACE_FILE"
+	done
+}
+
+function generate_configd {
+	setup_configd
+
+	echo "interface=$IF_LOCAL" > "$CONFIGD_IFACE_LOCAL"
+	echo "no-dhcp-interface=$IF_LOCAL" >> "$CONFIGD_IFACE_LOCAL"
+
+	if [[ "$IFACE" != "$IF_INTERNAL"* ]]; then
+		echo "Ignoring interface \`$IFACE': does not match \`$IF_INTERNAL'." | logger -t "$0"
+		return
+	fi
+
+	if [ "$(echo "$IF_NETMASK" | cut -d. -f3,4)" != "0.0" ]; then
+		echo "Ignoring interface \`$IFACE': unsupported netmask \`$IF_NETMASK'." | logger -t "$0"
+		return
+	fi
+
+	IF_NETWORK="$(echo "$IF_ADDRESS" | cut -d. -f1,2)"
+
+	echo "$IF_ADDRESS  $(hostname -s)-$IFACE-gw" >> "$CONFIGD_HOSTS"
+
+	echo "interface=$IFACE" > "$CONFIGD_IFACE"
+	echo "dhcp-range=$IF_ADDRESS,static,$IF_NETMASK" >> "$CONFIGD_IFACE"
+	echo "dhcp-range=$IF_NETWORK.128.1,$IF_NETWORK.255.254,1h" >> "$CONFIGD_IFACE"
+
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		echo "nat on $IF_EXTERNAL inet from ($IFACE:network) to any -> ($IF_EXTERNAL)" >> "$CONFIGD_PF"
+	fi
+}
+
+function osx_vbox_hostonlyif {
+	if [ -z "$VBOX_USER" ]; then
+		echo "VirtualBox configuration not possible. VBOX_USER not defined. Setup \`$LOCAL_CONFIG'." | logger -t "$0"
+		exit 1
+	fi
+
+	VBOX_KEXT_LOADED="no"
+
+	for i in {60..1}; do
+		if kextstat -lb org.virtualbox.kext.VBoxNetAdp 2>&1 | grep -q org.virtualbox.kext.VBoxNetAdp; then
+			VBOX_KEXT_LOADED="yes"
+			break
+		fi
+
+		sleep 1
+	done
+
+	if [ "$VBOX_KEXT_LOADED"  != "yes" ]; then
+		echo "VirtualBox kernel extension not loaded. Giving up." | logger -t "$0"
+		exit 1
+	fi
+
+	VBOX_IFLIST="$(sudo -Hnu "$VBOX_USER" VBoxManage list hostonlyifs)"
+	VBOX_INITIAL_SETUP="no"
+
+	if [ -z "$VBOX_IFLIST" ]; then
+		sudo -Hnu "$VBOX_USER" VBoxManage hostonlyif create 2>&1 | logger -t "$0"
+		VBOX_IFLIST="$(sudo -Hnu "$VBOX_USER" VBoxManage list hostonlyifs)"
+		VBOX_INITIAL_SETUP="yes"
+	fi
+
+	if [ -z "$VBOX_IFLIST" ]; then
+		echo "Error creating host-only network. Giving up." | logger -t "$0"
+		exit 1
+	fi
+
+	if [ "$VBOX_INITIAL_SETUP" == "yes" ]; then
+		IFACE="$(echo "$VBOX_IFLIST" | egrep '^Name:' | head -1 | awk '{ print $2 }')"
+
+		sudo -Hnu "$VBOX_USER" VBoxManage hostonlyif ipconfig "$IFACE" \
+			--ip "$IF_ADDRESS" --netmask "$IF_NETMASK" 2>&1 | logger -t "$0"
+
+		VBOX_IFLIST="$(sudo -Hnu "$VBOX_USER" VBoxManage list hostonlyifs)"
+	fi
+
+	IFACE=""
+	IF_ADDRESS=""
+	IF_NETMASK=""
+
+	for LINE in $(echo "$VBOX_IFLIST" | sed 's/ //g'); do
+		KEY="$(echo "$LINE" | cut -d: -f1)"
+		VALUE="$(echo "$LINE" | cut -d: -f2)"
+
+		case "$KEY" in
+			Name) IFACE="$VALUE" ;;
+			IPAddress) IF_ADDRESS="$VALUE" ;;
+			NetworkMask) IF_NETMASK="$VALUE" ;;
+
+			Status)
+				if [[ "$IFACE" == "$IF_INTERNAL"* ]]; then
+					sudo -Hnu "$VBOX_USER" VBoxManage hostonlyif ipconfig "$IFACE" \
+						--ip "$IF_ADDRESS" --netmask "$IF_NETMASK" 2>&1 | logger -t "$0"
+
+					sudo -Hnu "$VBOX_USER" VBoxManage dhcpserver modify \
+						--ifname "$IFACE" --disable 2>&1 | logger -t "$0"
+				fi
+
+				generate_configd
+
+				IFACE=""
+				IF_ADDRESS=""
+				IF_NETMASK=""
+				;;
+		esac
+	done
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	cleanup_configd
+	osx_vbox_hostonlyif
+
+	pfctl -f /etc/wznat/pf.conf -e 2>&1 | logger -t "$0"
+	launchctl unload /etc/wznat/dnsmasq.plist 2>&1 | logger -t "$0"
+	launchctl load /etc/wznat/dnsmasq.plist 2>&1 | logger -t "$0"
+	sysctl -w net.inet.ip.forwarding=1 2>&1 | logger -t "$0"
+else
+	cleanup_configd
+
+	[[ "$IFACE" != "$IF_INTERNAL"* ]] && exit 0
+
+	iptables -t nat -D POSTROUTING -o $IF_EXTERNAL -j MASQUERADE >/dev/null 2>&1
+	iptables -t mangle -D POSTROUTING -p udp --dport bootpc -o $IFACE -j CHECKSUM --checksum-fill >/dev/null 2>&1
+	service dnsmasq status >/dev/null 2>&1 && service dnsmasq restart
+
+	[ "$MODE" != "start" ] && exit 0
+
+	generate_configd
+
+	iptables -t nat -I POSTROUTING -o $IF_EXTERNAL -j MASQUERADE
+	iptables -t mangle -I POSTROUTING -p udp --dport bootpc -o $IFACE -j CHECKSUM --checksum-fill
+	service dnsmasq status >/dev/null 2>&1 && service dnsmasq restart
+
+	sysctl -qw net.ipv4.ip_forward=1
+fi
